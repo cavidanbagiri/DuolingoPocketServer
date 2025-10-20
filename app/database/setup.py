b2b3f1f2f77c7bdc137f807d686f9974c0774cfa
= <=>
2025-10-20 15:59:12 +0300

    Restore database to render

--- app/database/setup.py
+++ app/database/setup.py
@@ -51,11 +51,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get database credentials
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
+# DB_USER = os.getenv('DB_USER')
+# DB_PASSWORD = os.getenv('DB_PASSWORD')
+# DB_HOST = os.getenv('DB_HOST')
+# DB_PORT = os.getenv('DB_PORT')
+# DB_NAME = os.getenv('DB_NAME')
+
+DB_USER = os.getenv('RENDER_DB_USER')
+DB_PASSWORD = os.getenv('RENDER_DB_PASSWORD')
+DB_HOST = os.getenv('RENDER_DB_HOST')
+DB_PORT = os.getenv('RENDER_DB_PORT')
+DB_NAME = os.getenv('RENDER_DB_NAME')
 
 
 connection_string = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
